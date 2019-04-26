673e14895b7adfd63bc840b7a3163bfebecfd7e1
aditya pratap singh <pratapaditya1997@gmail.com>
2019-04-26 17:54:47 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,59 +1,26 @@
-#include "bits/stdc++.h"
-#define ll long long int
-#define all(x) x.begin(), x.end()
-#define pb push_back
-#define se second
-#define fi first
-using namespace std;
-typedef pair<ll,ll> pll;
+const int N = 3e6+5;
+int trie[N][2], cnt[N],sz=0;
 
-const int N = 1e5+5;
-ll trie[60*N][2],sz=0;
-
-void insert(ll val){
-    ll curr=0;
-    for(ll i=45;i>=0;i--){
-        ll dir=0;
-        if((val>>i)&1) dir=1;
-        if(trie[curr][dir]==-1) trie[curr][dir]=++sz;
-        curr=trie[curr][dir];
+void update(int num,int dx) { // num to be inserted and dx is the increment to be made in the count 
+    int node = 0;
+    for(int i=30;i>=0;i--) {
+        int bit = (num >> i) & 1;
+        if(!trie[node][bit]){
+            trie[node][bit] += ++sz;
+        }
+        node = trie[node][bit];
+        cnt[node] += dx;
     }
 }
 
-ll max_xor_query(ll val){
-    ll ans=0,curr=0;
-    for(ll i=45;i>=0;i--){
-        ll dir=0;
-        if((val>>i)&1) dir=1;
-        if(dir==0){
-            if(trie[curr][1]!=-1){
-                ans += (1LL<<i);
-                curr=trie[curr][1];
-            }
-            else{
-                assert(trie[curr][0]!=-1);
-                curr=trie[curr][0];
-            }
-        }
-        if(dir==1){
-            if(trie[curr][0]!=-1){
-                ans += (1LL<<i);
-                curr=trie[curr][0];
-            }
-            else{
-                assert(trie[curr][1]!=-1);
-                curr=trie[curr][1];
-            }
-        }
+int max_xor_query(int num) {
+    int ret = 0, node = 0;
+    for(int i=30;i>=0;i--) {
+        int bit = (num >> i) & 1;
+        if(cnt[trie[node][bit^1]]) {
+            ret |= (1<<i);
+            node = trie[node][bit^1];
+        } else node = trie[node][bit];
     }
-    return ans;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    memset(trie,-1,sizeof trie);
-    insert(5); insert(10);
-    cout<<max_xor_query(6)<<endl;
-    return 0;
+    return ret;
 }
